1ecddb48e388b4b01086514b5e8f35e86fd3fd03
Leon Zhao <leonzhao2020@outlook.com>
2020-07-08 19:30:05 +0800

    4nd_update

--- .up2git.sh
+++ .up2git.sh
@@ -1,5 +1,10 @@
 git add -A
 #git config --global user.email "leonzhao2020@outlook.com"
 #git config --global user.name "Leon Zhao"
-git commit -m '2nd submission'
-git push
+if [ "$#" -eq 1 ]; then
+  comment=$1
+  git commit -m $comment
+  git push origin HEAD:master
+else
+  echo "usage: .up2git.sh '<comment_here>' ";exit 2
+fi
